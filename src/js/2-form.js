39f1b3380a55a02000e03a16ec40597208a79905
Oleksandr Pavlenko <aleksandr.pavlenko2016@gmail.com>
2026-04-21 07:47:41 +0300

    HW JS 9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,46 @@
-console.log("2+2");
+const formData = {
+    email: "",
+    message: ""
+};
+
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = "feedback-form-state";
+
+populateForm();
+
+form.addEventListener('input', (event) => {
+    const { name, value } = event.target;
+    
+    formData[name] = value.trim();
+    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (formData.email === "" || formData.message === "") {
+        alert("Fill please all fields");
+        return;
+    }
+
+    console.log(formData);
+    
+    localStorage.removeItem(STORAGE_KEY);
+    formData.email = "";
+    formData.message = "";
+    form.reset();
+});
+
+function populateForm() {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+
+    if (savedData) {
+        const parsedData = JSON.parse(savedData);
+        
+        Object.assign(formData, parsedData);
+        
+        form.elements.email.value = formData.email;
+        form.elements.message.value = formData.message;
+    }
+}
